fe1ebd670d43498650ca3972965454836ca3aef8
Nhat Phan <nhat.phan@ntworld.net>
2020-02-24 20:28:27 +0100

    [UI] Remove left border of JBTabs

--- merge-request-integration-core/src/main/kotlin/net/ntworld/mergeRequestIntegrationIde/ui/util/Tabs.kt
+++ merge-request-integration-core/src/main/kotlin/net/ntworld/mergeRequestIntegrationIde/ui/util/Tabs.kt
@@ -1,11 +1,15 @@
 package net.ntworld.mergeRequestIntegrationIde.ui.util
 
-import com.intellij.execution.ui.layout.impl.JBRunnerTabs
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.actionSystem.ActionGroup
-import com.intellij.ui.tabs.JBTabs
-import com.intellij.ui.tabs.TabInfo
-import com.intellij.ui.tabs.TabsListener
+import com.intellij.openapi.actionSystem.ActionManager
+import com.intellij.openapi.wm.IdeFocusManager
+import com.intellij.ui.awt.RelativePoint
+import com.intellij.ui.tabs.*
+import com.intellij.ui.tabs.impl.*
+import com.intellij.ui.tabs.impl.singleRow.ScrollableSingleRowLayout
+import com.intellij.ui.tabs.impl.singleRow.SingleRowLayout
+import java.awt.*
 import javax.swing.JComponent
 import com.intellij.openapi.project.Project as IdeaProject
 
@@ -13,7 +17,7 @@ class Tabs(
     private val ideaProject: IdeaProject,
     private val disposable: Disposable
 ) : TabsUI {
-    private val myTabs = JBRunnerTabs.create(ideaProject, disposable)
+    private val myTabs: JBTabs = MyTabs(ideaProject, disposable)
     private var myCommonActionGroupInCenterFactory: (() -> ActionGroup)? = null
     private var myCommonSideComponentFactory: (() -> JComponent)? = null
 
@@ -39,5 +43,85 @@ class Tabs(
 
     override fun addListener(listener: TabsListener) {
         myTabs.addListener(listener)
+    }
+
+    private class MyTabs(
+        private val ideaProject: IdeaProject,
+        private val disposable: Disposable
+    ) : SingleHeightTabs(
+        ideaProject,
+        ActionManager.getInstance(),
+        IdeFocusManager.getInstance(ideaProject),
+        disposable
+    ) {
+        override fun createTabPainterAdapter(): TabPainterAdapter? {
+            return DefaultTabPainterAdapter(JBTabPainter.DEBUGGER)
+        }
+
+        override fun createSingleRowLayout(): SingleRowLayout? {
+            return ScrollableSingleRowLayout(this)
+        }
+
+        override fun createTabBorder(): JBTabsBorder? {
+            return JBRunnerTabsBorder(this)
+        }
+
+        override fun useSmallLabels(): Boolean {
+            return true
+        }
+
+        override fun getToolbarInset(): Int {
+            return 0
+        }
+
+        fun shouldAddToGlobal(point: Point?): Boolean {
+            val label = selectedLabel
+            if (label == null || point == null) {
+                return true
+            }
+            val bounds = label.bounds
+            return point.y <= bounds.y + bounds.height
+        }
+
+        override fun layout(c: JComponent?, bounds: Rectangle): Rectangle? {
+            if (c is Toolbar) {
+                bounds.height -= separatorWidth
+                return super.layout(c, bounds)
+            }
+            return super.layout(c, bounds)
+        }
+
+        override fun processDropOver(over: TabInfo?, relativePoint: RelativePoint) {
+            val point = relativePoint.getPoint(component)
+            myShowDropLocation = shouldAddToGlobal(point)
+            super.processDropOver(over, relativePoint)
+            for ((key, label) in myInfo2Label) {
+                if (label.bounds.contains(point) && myDropInfo != key) {
+                    select(key, false)
+                    break
+                }
+            }
+        }
+
+        private class JBRunnerTabsBorder(private val jbTabs: JBTabsImpl) : JBTabsBorder(jbTabs) {
+            override val effectiveBorder: Insets
+                get() = Insets(jbTabs.borderThickness, 0, 0, 0)
+
+            override fun paintBorder(
+                c: Component,
+                g: Graphics,
+                x: Int,
+                y: Int,
+                width: Int,
+                height: Int
+            ) {
+                if (jbTabs.isEmptyVisible) return
+                jbTabs.tabPainter.paintBorderLine(
+                    g as Graphics2D, jbTabs.borderThickness, Point(x, y + jbTabs.myHeaderFitSize.height),
+                    Point(x + width, y + jbTabs.myHeaderFitSize.height)
+                )
+            }
+
+        }
     }
 }
