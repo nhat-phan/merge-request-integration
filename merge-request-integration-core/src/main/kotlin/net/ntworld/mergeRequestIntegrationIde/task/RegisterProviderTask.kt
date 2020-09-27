17da85bf6a1ccdfe85998ecf7d8705c3bc6b9a97
regevbr <regevbr@gmail.com>
2020-09-27 17:48:43 +0300

    fix #95 - Hanging background tasks "fetching"

--- merge-request-integration-core/src/main/kotlin/net/ntworld/mergeRequestIntegrationIde/task/RegisterProviderTask.kt
+++ merge-request-integration-core/src/main/kotlin/net/ntworld/mergeRequestIntegrationIde/task/RegisterProviderTask.kt
@@ -13,54 +13,54 @@ import net.ntworld.mergeRequestIntegrationIde.infrastructure.ProjectServiceProvi
 import net.ntworld.mergeRequestIntegrationIde.infrastructure.ProviderSettings
 
 class RegisterProviderTask(
-        private val projectServiceProvider: ProjectServiceProvider,
-        private val id: String,
-        private val name: String,
-        private val settings: ProviderSettings,
-        private val listener: Listener
+    private val projectServiceProvider: ProjectServiceProvider,
+    private val id: String,
+    private val name: String,
+    private val settings: ProviderSettings,
+    private val listener: Listener
 ) : Task.Backgroundable(projectServiceProvider.project, "Fetching provider information...", true) {
 
-    fun start() {
-        ProgressManager.getInstance().runProcessWithProgressAsynchronously(
-                this,
-                Indicator(this)
-        )
-    }
+  fun start() {
+    ProgressManager.getInstance().runProcessWithProgressAsynchronously(
+        this,
+        Indicator(this)
+    )
+  }
 
-    override fun run(indicator: ProgressIndicator) {
-        val job = GlobalScope.launch {
-            val pair = projectServiceProvider.providerStorage.register(
-                    infrastructure = projectServiceProvider.infrastructure,
-                    id = id,
-                    key = settings.id,
-                    name = name,
-                    info = settings.info,
-                    credentials = settings.credentials,
-                    repository = settings.repository
-            )
-            if (!indicator.isCanceled) {
-                listener.providerRegistered(pair.first)
-                if (pair.first.status == ProviderStatus.ERROR) {
-                    projectServiceProvider.notify(pair.first.errorMessage ?: "", NotificationType.ERROR)
-                }
-                if (null !== pair.second) {
-                    throw pair.second!!
-                }
-            }
+  override fun run(indicator: ProgressIndicator) {
+    val job = GlobalScope.launch {
+      val pair = projectServiceProvider.providerStorage.register(
+          infrastructure = projectServiceProvider.infrastructure,
+          id = id,
+          key = settings.id,
+          name = name,
+          info = settings.info,
+          credentials = settings.credentials,
+          repository = settings.repository
+      )
+      if (!indicator.isCanceled) {
+        listener.providerRegistered(pair.first)
+        if (pair.first.status == ProviderStatus.ERROR) {
+          projectServiceProvider.notify(pair.first.errorMessage ?: "", NotificationType.ERROR)
         }
-        job.start()
-        while (job.isActive && !indicator.isCanceled) {
-            Thread.sleep(100);
+        if (null !== pair.second) {
+          throw pair.second!!
         }
-        if (job.isActive) {
-            job.cancel()
-        }
-        indicator.checkCanceled()
+      }
     }
-
-    private class Indicator(private val task: RegisterProviderTask) : BackgroundableProcessIndicator(task)
-
-    interface Listener {
-        fun providerRegistered(providerData: ProviderData)
+    job.start()
+    while (job.isActive && !indicator.isCanceled) {
+      Thread.sleep(100);
     }
+    if (job.isActive) {
+      job.cancel()
+    }
+    indicator.checkCanceled()
+  }
+
+  private class Indicator(private val task: RegisterProviderTask) : BackgroundableProcessIndicator(task)
+
+  interface Listener {
+    fun providerRegistered(providerData: ProviderData)
+  }
 }
